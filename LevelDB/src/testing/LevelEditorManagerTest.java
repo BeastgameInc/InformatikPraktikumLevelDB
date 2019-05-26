dfb60d5c7e86d9b657ef18092e3db8cebeb9b12d
Carina <Carina@192.168.0.17>
2019-05-26 22:33:27 +0200

    Edit JUnit testcases for LevelEditorManager.

--- LevelDB/src/testing/LevelEditorManagerTest.java
+++ LevelDB/src/testing/LevelEditorManagerTest.java
@@ -1,11 +1,43 @@
 package testing;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import org.junit.Test;
 
+import functionell.LevelEditorManager;
+import junit.framework.Assert;
+import structs.TileInfoSTR;
+
 public class LevelEditorManagerTest {
+	LevelEditorManager myLevelEditorManager = new LevelEditorManager();
 	@Test
-	public void derTest1() {
-		
+	public void derTest1() throws IllegalArgumentException, IllegalAccessException {
+		myLevelEditorManager.addTile(new TileInfoSTR(0,0,"Vordergrund"));
+		myLevelEditorManager.addTile(new TileInfoSTR(1,1,"Vordergrund"));
+		myLevelEditorManager.addTile(new TileInfoSTR(2,2,"Vordergrund"));
+		String umwandeln;
+		umwandeln = myLevelEditorManager.getJSONTiles().toString();
+		System.out.println(umwandeln);
+		Assert.assertEquals("[{\"x\":0,\"y\":0,\"Layer\":\"Vordergrund\"},{\"x\":1,\"y\":1,\"Layer\":\"Vordergrund\"},{\"x\":2,\"y\":2,\"Layer\":\"Vordergrund\"}]", umwandeln);
+
+	}
+	
+	@Test
+	public void derTest2() throws IOException {
+		BufferedImage img = ImageIO.read(new File("testimage.png"));
+		BufferedImage[] aer = myLevelEditorManager.cutSpritesheet(img);
+		Assert.assertNotNull(aer);
+	}
+	
+	@Test
+	public void derTest3( ) throws IOException {
+		myLevelEditorManager.setSubSpritesFile("testimage.png");
+		Assert.assertNotNull(myLevelEditorManager.getsubSprites());
 	}
 
 }
+
