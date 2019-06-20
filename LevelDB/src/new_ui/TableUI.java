b6b2a98ab51ca3730fdad92a148d43228b087fd3
temouttatem <50463827+temouttatem@users.noreply.github.com>
2019-06-20 12:01:46 +0200

    Neue UI in Netbeans

---
+++ LevelDB/src/new_ui/TableUI.java
@@ -0,0 +1,20 @@
+
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+class TableUI {
+
+    static DefaultTableModel getModel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
