a9cd2635aa13ffdc043b579fc2ff26e95a99ca4e
BeastgameTL <38040513+BeastgameTL@users.noreply.github.com>
2019-06-23 15:37:51 +0200

    Updated Ui-classes

--- LevelDB/src/levelDBrepack/MainApp.java
+++ LevelDB/src/levelDBrepack/MainApp.java
@@ -1,16 +1,88 @@
 package levelDBrepack;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import levelDBrepack.model.LevelProp;
+import levelDBrepack.view.LevelTableController;
 
 public class MainApp extends Application {
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	private ObservableList<LevelProp> levelData = FXCollections.observableArrayList();
+	
+	public MainApp() {
+		// TODO maybe todo constructor for mainApp
+	}
+	
+	public ObservableList<LevelProp> getLevelData() {
+		return levelData;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("LEVELDB");
 		
+		initRootLayout();
+		
+		showLevelTable();
 	}
+
+	private void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void showLevelTable() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/LevelTable.fxml"));
+			AnchorPane levelTable = (AnchorPane) loader.load();
+			
+			rootLayout.setCenter(levelTable);
+			
+			LevelTableController controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	public Window getPrimaryStage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean showLevelEditDialog(LevelProp tempLevel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 }
--- LevelDB/src/levelDBrepack/view/LevelTableController.java
+++ LevelDB/src/levelDBrepack/view/LevelTableController.java
@@ -1,9 +1,94 @@
 package levelDBrepack.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import levelDBrepack.MainApp;
+import levelDBrepack.model.LevelProp;
 
 public class LevelTableController {
 	
+	@FXML
+	private TableView<LevelProp> levelTable;
 	
+	@FXML
+	private TableColumn<LevelProp, String> idColumn;
+	@FXML
+	private TableColumn<LevelProp, Integer> widthColumn;
+	@FXML
+	private TableColumn<LevelProp, Integer> heightColumn;
+	@FXML
+	private TableColumn<LevelProp, String> contentColumn; 
+	
+	
+	private MainApp mainApp;
+	
+	public LevelTableController() {	
+		
+	}
+	
+	@FXML
+	private void initialize() {
+		idColumn.setCellValueFactory(cellData -> cellData.getValue().idProperty());
+		widthColumn.setCellValueFactory(cellData -> cellData.getValue().widthProperty().asObject());
+		heightColumn.setCellValueFactory(cellData -> cellData.getValue().heightProperty().asObject());
+		contentColumn.setCellValueFactory(cellData -> cellData.getValue().propsProperty());
+		
+		
+		
+		//levelTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> show);
+	}
+	
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+		levelTable.setItems(mainApp.getLevelData());
+	}
+	
+	@FXML
+	private void handleDeleteLevel() {
+		int selectedIndex = levelTable.getSelectionModel().getSelectedIndex();
+		if(selectedIndex >= 0) {
+			levelTable.getItems().remove(selectedIndex);
+		} else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.initOwner(mainApp.getPrimaryStage());
+			alert.setTitle("No Selection");
+			alert.setHeaderText("No Person Selected");
+			alert.setContentText("Please select a level.");
+			
+			alert.showAndWait();
+		}
+	}
+	
+	@FXML
+	private void handleNewLevel() {
+		LevelProp tempLevel = new LevelProp();
+		boolean okClicked = mainApp.showLevelEditDialog(tempLevel);
+		if(okClicked) {
+			mainApp.getLevelData().add(tempLevel);
+		}
+	}
+	
+	@FXML
+	private void handleEditPerson() {
+		LevelProp selectedPerson = levelTable.getSelectionModel().getSelectedItem();
+	    if (selectedPerson != null) {
+	        boolean okClicked = mainApp.showLevelEditDialog(selectedPerson);
+	        if (okClicked) {
+	            //showLevelDetails(selectedPerson); //TODO see if mistake
+	        }
+
+	    } else {
+	        // Nothing selected.
+	        Alert alert = new Alert(AlertType.WARNING);
+	        alert.initOwner(mainApp.getPrimaryStage());
+	        alert.setTitle("No Selection");
+	        alert.setHeaderText("No Level Selected");
+	        alert.setContentText("Please select a level.");
+
+	        alert.showAndWait();
+	    }
+	}
 }
